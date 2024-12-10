ea715b4bebd3ac2e61ac50bcd00f7d77d4bfadbb
Sunkeshari Deshar <desarleah@gmail.com>
2024-12-10 03:22:32 -0600

    updated gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn main:app
+web: gunicorn main:app -b 127.0.0.1:5000
+
--- main.py
+++ main.py
@@ -301,4 +301,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False,port=5000)
